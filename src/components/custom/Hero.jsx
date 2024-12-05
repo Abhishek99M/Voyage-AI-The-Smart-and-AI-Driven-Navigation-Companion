98edb6ea27052b7aa585da0d6ecb357724cec72e
Abhishek Kumar <110620295+Abhishek99M@users.noreply.github.com>
2024-12-05 15:31:44 +0530

    f

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <div className='flex flex-col items-center px-6 md:px-20 lg:px-36 gap-6 md:gap-10'>
-      <h1 className='font-extrabold text-3xl md:text-4xl lg:text-6xl text-center mt-12 leading-tight lg:leading-snug'>
-        <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your Fingertips</h1>
+      <h1 className='font-extrabold text-3xl md:text-4xl xl:text-6xl text-center mt-12 leading-tight lg:leading-snug'>
+        <span className='text-[#f56551]'>Discover Your Next Adventure with AI: </span> 
+        Personalized Itineraries at Your Fingertips</h1>
       <p className='text-base md:text-lg lg:text-xl text-gray-600 text-center max-w-4xl'>Your personal trip planner and travel curator, creating custom Itineraries tailored to your interests and budget.</p>
       
 
